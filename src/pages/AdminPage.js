495753d48ed5ac6b91e635eb1333a5d2bbcba449
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2024-04-23 22:19:50 +0000

    Fix: import useEffect

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LayoutAdmin from '../components/LayoutAdmin';
 import { getTeachers, saveTeachers } from '../services/professorService';
 
